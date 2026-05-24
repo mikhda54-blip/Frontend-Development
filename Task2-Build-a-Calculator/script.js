8335857d25c6feb2479188800d4fe4003ba07f5d
mikhda54-blip <mikhda54@gmail.com>
2026-05-25 00:14:39 +0530

    Add files via upload

---
+++ Task2-Build-a-Calculator/script.js
@@ -0,0 +1,12 @@
+function addValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    document.getElementById("display").value =
+    eval(document.getElementById("display").value);
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
